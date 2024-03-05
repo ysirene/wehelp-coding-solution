e0c5a10194744af2e07b0bbc273b24241a649628
yashan <leeys2423@gmail.com>
2024-03-06 00:54:39 +0800

    Time: 16.50 ms

--- 04-find-second-largest-num/04-find-second-largest-num.py
+++ 04-find-second-largest-num/04-find-second-largest-num.py
@@ -1,6 +1,9 @@
 def findSecond(nums):
-    nums_set = set(nums)
-    sorted_nums = sorted(list(nums_set), reverse=True)
-    return sorted_nums[1]
-
-print(findSecond([1, 3, 3, 2, 5, -2]))
+    max_num = max(nums[0], nums[1])
+    second_max_num = min(nums[0], nums[1])
+    for i in range(2, len(nums)):
+        if nums[i] > max_num:
+            max_num, second_max_num = nums[i], max_num
+        elif nums[i] < max_num and nums[i] > second_max_num:
+            second_max_num = nums[i]
+    return second_max_num
